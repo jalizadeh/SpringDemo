8005d811333f6103aeb9d8adac3bcf18fd3bb67a
Javad <jalizade3d@yahoo.com>
2018-11-14 09:52:06 +0100

    BeanFactory added'

--- src/com/jalizadeh/Main.java
+++ src/com/jalizadeh/Main.java
@@ -1,11 +1,37 @@
 package com.jalizadeh;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+
+/**
+ * 
+ * @author Javad Alizadeh
+ * @see {@link https://www.youtube.com/watch?v=7c6ZTF6cF88&list=PLGibysfsUS7NAbefiaj1V4LbX0glTftDI}
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
 
-		Triangle triangle = new Triangle();
+		//Triangle triangle = new Triangle();
+		
+		/*
+		@Deprecated
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		*/
+		
+		//xml must be located in "src" folder
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		//getBean by Id
+		Triangle triangle = (Triangle) context.getBean("triangle");		
 		triangle.draw();
+		
+		//to prevent resource leak
+		context.close();
 
 	}
 
