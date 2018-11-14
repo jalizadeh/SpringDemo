d937afcb52dd7052e13c5bdf3d9f54bc4e9872d4
Javad <jalizade3d@yahoo.com>
2018-11-14 12:29:39 +0100

    Construction Injection - #6

--- src/com/jalizadeh/Triangle.java
+++ src/com/jalizadeh/Triangle.java
@@ -16,9 +16,36 @@ public class Triangle {
 		this.type = type;
 	}
 
+	//#5------------
 
 
+	//#6
+	private int height;
+	private int length;
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getLenght() {
+		return length;
+	}
+	
+	public Triangle(int height) {
+		this.height = height;
+	}
+	
+	public Triangle(String type) {
+		this.type = type;
+	}
+	
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+	//#6---------------------
+	
 	public void draw() {
-		System.out.println(getType() + " Triangle drawn");
+		System.out.println(getType() + " Triangle drawn " + getHeight());
 	}
 }
