d9f4d97dbb8e6d06f677bc8b7592632e29f0afa9
Javad <jalizade3d@yahoo.com>
2018-11-14 09:54:25 +0100

    Property - #5

--- src/com/jalizadeh/Triangle.java
+++ src/com/jalizadeh/Triangle.java
@@ -2,7 +2,23 @@ package com.jalizadeh;
 
 public class Triangle {
 	
+	//video #5
+	private String type;
+	
+	
+	public String getType() {
+		return type;
+	}
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
 	public void draw() {
-		System.out.println("Triangle drawn");
+		System.out.println(getType() + " Triangle drawn");
 	}
 }
