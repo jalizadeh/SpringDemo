349ec128612a08cf97bce4ac7af2f21412fb7837
Javad <jalizade3d@yahoo.com>
2018-11-15 17:21:53 +0100

    Bean Definition Inheritance - #13

--- src/com/jalizadeh/Main.java
+++ src/com/jalizadeh/Main.java
@@ -1,9 +1,6 @@
 package com.jalizadeh;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 
 
 /**
@@ -15,23 +12,13 @@ import org.springframework.core.io.FileSystemResource;
 public class Main {
 
 	public static void main(String[] args) {
-
-		//Triangle triangle = new Triangle();
-		
-		/*
-		@Deprecated
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		*/
-		
 		//xml must be located in "src" folder
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		
 		//getBean by Id
-		//Triangle triangle = (Triangle) context.getBean("triangle");		
+		Triangle triangle = (Triangle) context.getBean("triangle");		
 		
-		//or using alias
-		Triangle triangle = (Triangle) context.getBean("triangle-alias");
-		//triangle.draw();
+
 		triangle.drawPoints();
 		
 		//to prevent resource leak
--- src/com/jalizadeh/Triangle.java
+++ src/com/jalizadeh/Triangle.java
@@ -7,65 +7,9 @@ import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-						//video #12
-public class Triangle implements ApplicationContextAware, BeanNameAware{
-	
-	
-	
-	/*
-	//video #5
-	private String type;
-	
-	
-	public String getType() {
-		return type;
-	}
 
-
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	//#5------------
-
-
-	//#6
-	private int height;
-	private int length;
-	
-	public int getHeight() {
-		return height;
-	}
-	
-	public int getLenght() {
-		return length;
-	}
-	
-	public Triangle(int height) {
-		this.height = height;
-	}
-	
-	public Triangle(String type) {
-		this.type = type;
-	}
-	
-	public Triangle(String type, int height) {
-		this.type = type;
-		this.height = height;
-	}
-	//#6---------------------
-	
-	
-	public void draw() {
-		System.out.println(getType() + " Triangle drawn " + getHeight());
-	}
-	
-	*/
-	
-	
-	//video #7 & #9
-	/*private Point point0;
+public class Triangle {
+	private Point point0;
 	private Point point1;
 	private Point point2;
 	
@@ -112,70 +56,6 @@ public class Triangle implements ApplicationContextAware, BeanNameAware{
 		System.out.println("B: " + point1);
 		System.out.println("C: " + point2);
 	}
-	*/
 	
-	
-	
-	//video #10
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
-	public Point getPointA() {
-		return pointA;
-	}
-	public Point getPointB() {
-		return pointB;
-	}
-	public Point getPointC() {
-		return pointC;
-	}
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
-	}
-	public void setPointB(Point pointB) {
-		this.pointB = pointB;
-	}
-	public void setPointC(Point pointC) {
-		this.pointC = pointC;
-	}
-	
-	public void drawPoints() {
-		System.out.println("A: " + pointA);
-		System.out.println("B: " + pointB);
-		System.out.println("C: " + pointC);
-	}
-	
-	//video #12 
-	private ApplicationContext context;
-	
-	@Override
-	public void setApplicationContext(ApplicationContext context) throws BeansException {
-		this.context = context;
-		//System.out.println("context: " + context.getDisplayName());
-	}
-	@Override
-	public void setBeanName(String beanName) {
-		System.out.println("Bean name is: " + beanName);
-		
-	}
-	
-	
-	
-	/*
-	private List<Point> points;
 
-	public List<Point> getPoints() {
-		return points;
-	}
-
-	public void setPoints(List<Point> points) {
-		this.points = points;
-	}
-	
-	public void drawPoints() {
-		for(Point p : points)
-			System.out.println("point: " + p);
-		
-	}
-	*/
 }
